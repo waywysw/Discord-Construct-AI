0513a9b746a77c2acb62155ee7e876c2f762ce2f
thesentinel2615 <nevyn202@outlook.com>
2023-07-19 10:19:17 -0500

    Update Card.js

--- src/discord/commands/Card.js
+++ src/discord/commands/Card.js
@@ -11,19 +11,47 @@ const command = {
 	async execute(interaction) {
         let character = await getCharacter(botSettings.charId);
         const attachment = new AttachmentBuilder(`${CHARACTER_IMAGES_FOLDER}${character.avatar}`, {name: `${character.name}.png`});
-        let embedArray = [                { name: 'Greeting', value: `${character.first_mes || 'None'}`, inline: false },
-        { name: 'Description', value: `${character.description || 'None'}`, inline: false },
-        { name: 'Scenario', value: `${character.scenario || 'None'}`, inline: false },];
-        if(character.mes_examples.length > 200){
-            embedArray.push({ name: 'Examples', value: `${character.mes_examples.slice(0, 200)}...`, inline: false });
-            embedArray.push({ name: 'Examples Continued', value: `${character.mes_examples.slice(200)}`, inline: false });
+        let embedArray = [];
+        if(character.first_mes.length > 800){
+            embedArray.push({ name: 'Greeting', value: `${character.first_mes.slice(0, 800)}`, inline: false });
+            embedArray.push({ name: 'Greeting Continued', value: `${character.first_mes.slice(800, 1600)}`, inline: false });
+            if(character.first_mes.length > 1600){
+                embedArray.push({ name: 'Greeting Continued', value: `${character.first_mes.slice(1600)}`, inline: false });
+            }
         }else{
-            embedArray.push({ name: 'Examples', value: `${character.mes_examples || 'None'}`, inline: false });
+            embedArray.push({ name: 'Greeting', value: `${character.first_mes || 'None'}`, inline: false });
+        }
+        if(character.description.length > 800){
+            embedArray.push({ name: 'Description', value: `${character.description.slice(0, 800)}`, inline: false });
+            embedArray.push({ name: 'Description Continued', value: `${character.description.slice(800, 1600)}`, inline: false });
+            if(character.description.length > 1600){
+                embedArray.push({ name: 'Description Continued', value: `${character.description.slice(1600)}`, inline: false });
+            }
+        }else{
+            embedArray.push({ name: 'Description', value: `${character.description || 'None'}`, inline: false });
+        }
+        if(character.scenario.length > 800){
+            embedArray.push({ name: 'Scenario', value: `${character.scenario.slice(0, 800)}`, inline: false });
+            embedArray.push({ name: 'Scenario Continued', value: `${character.scenario.slice(800, 1600)}`, inline: false });
+            if(character.scenario.length > 1600){
+                embedArray.push({ name: 'Scenario Continued', value: `${character.scenario.slice(1600)}`, inline: false });
+            }
+        }else{
+            embedArray.push({ name: 'Scenario', value: `${character.scenario || 'None'}`, inline: false });
+        }
+        if(character.mes_example.length > 800){
+            embedArray.push({ name: 'Examples', value: `${character.mes_example.slice(0, 800)}`, inline: false });
+            embedArray.push({ name: 'Examples Continued', value: `${character.mes_example.slice(800, 1600)}`, inline: false });
+            if(character.mes_example.length > 1600){
+                embedArray.push({ name: 'Examples Continued', value: `${character.mes_example.slice(1600)}`, inline: false });
+            }
+    
+        }else{
+            embedArray.push({ name: 'Examples', value: `${character.mes_example || 'None'}`, inline: false });
         }
         let embed = new EmbedBuilder()
             .setTitle(character.name)
-            .addFields(
-            )
+            .addFields(embedArray)
             .setImage(`attachment://${character.name}.png`)
             .setColor(0x0099FF)
         await interaction.reply({ embeds: [embed], files: [attachment]});
